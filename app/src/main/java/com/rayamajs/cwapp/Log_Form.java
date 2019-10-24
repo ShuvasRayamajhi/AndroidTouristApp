afc51819ac235721c91315b16c00589d27e85075
Shuvas Rayamajhi <rayamajs@coventry.ac.uk>
2019-10-24 12:11:34 +0100

    material design layout done for log in form

--- app/src/main/java/com/rayamajs/cwapp/Log_Form.java
+++ app/src/main/java/com/rayamajs/cwapp/Log_Form.java
@@ -2,7 +2,9 @@ package com.rayamajs.cwapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Log_Form extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Log_Form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log__form);
+        getSupportActionBar().setTitle("Login Form");
+    }
+
+    public void btn_signForm(View view) {
+
+        startActivity(new Intent(getApplicationContext(), Sign_Form.class));
     }
 }
