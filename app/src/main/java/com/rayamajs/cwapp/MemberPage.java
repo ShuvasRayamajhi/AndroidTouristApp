4ef2029a41bac3ded264978d7528e2d1197b9908
rayamajs <rayamajs@coventry.ac.uk>
2019-12-04 20:35:59 +0000

    required intents added

--- app/src/main/java/com/rayamajs/cwapp/MemberPage.java
+++ app/src/main/java/com/rayamajs/cwapp/MemberPage.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
+
+import java.util.Locale;
 
 public class MemberPage extends AppCompatActivity {
 
@@ -20,4 +23,14 @@ public class MemberPage extends AppCompatActivity {
     }
 
 
+    public void btnSpeech(View view) {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Say Something");
+        startActivityForResult(intent, 1);
+
+    }
 }
