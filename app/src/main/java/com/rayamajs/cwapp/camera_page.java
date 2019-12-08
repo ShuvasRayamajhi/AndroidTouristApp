a46b57556cc08e704f2fc830b955018ee2b9fe49
rayamajs <rayamajs@coventry.ac.uk>
2019-12-08 17:41:43 +0000

    on click listener image capture

--- app/src/main/java/com/rayamajs/cwapp/camera_page.java
+++ app/src/main/java/com/rayamajs/cwapp/camera_page.java
@@ -2,8 +2,11 @@ package com.rayamajs.cwapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -11,7 +14,6 @@ public class camera_page extends AppCompatActivity {
 
     Button btnCaptureImage;
     ImageView imageDisplay;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +23,15 @@ public class camera_page extends AppCompatActivity {
         btnCaptureImage = (Button) findViewById(R.id.btn_captureImage);
         imageDisplay = (ImageView) findViewById(R.id.imageCapture);
 
+       btnCaptureImage.setOnClickListener(new View.OnClickListener(){
+
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+               startActivityForResult(intent, 0);
+           }
+       });
     }
+
+
 }
