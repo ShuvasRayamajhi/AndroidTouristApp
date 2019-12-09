991ce1dab7ad70db5c7f873f641b01c45f6e58a8
rayamajs <rayamajs@coventry.ac.uk>
2019-12-09 21:23:19 +0000

    creating a name for the file

--- app/src/main/java/com/rayamajs/cwapp/photo_camera.java
+++ app/src/main/java/com/rayamajs/cwapp/photo_camera.java
@@ -3,7 +3,15 @@ package com.rayamajs.cwapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.io.File.createTempFile;
 
 public class photo_camera extends AppCompatActivity {
 
@@ -16,10 +24,20 @@ public class photo_camera extends AppCompatActivity {
 
     public void captureImage(View view) {
 
+
     }
 
     public void displayImage(View view) {
     }
 
+    private File getImageFile () throws IOException
+    {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageName = "jpg_" +timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
+        File imageFile = File.createTempFile(imageName, ".jpeg", storageDir);
+        currentImagePath = imageFile.getAbsolutePath();
+        return imageFile;
+    }
 }
