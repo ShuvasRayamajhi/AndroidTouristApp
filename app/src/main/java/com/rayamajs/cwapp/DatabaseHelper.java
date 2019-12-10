33110e7b9d95261f75fdb6742cd75e95cd388268
rayamajs <rayamajs@coventry.ac.uk>
2019-12-10 00:43:46 +0000

    api bug fix

--- app/src/main/java/com/rayamajs/cwapp/DatabaseHelper.java
+++ app/src/main/java/com/rayamajs/cwapp/DatabaseHelper.java
@@ -4,10 +4,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-
+    //declaring database, table and columns
     public static final String DATABASE_NAME = "Review.db";
     public static final String TABLE_NAME = "review_table";
     public static final String COL_1 = "ID";
@@ -15,20 +14,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_3 = "REVIEW";
     public static final String COL_4 = "RATING";
 
-    public DatabaseHelper(@Nullable Context context) {
-        super(context, DATABASE_NAME , null, 1);
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1); //create database
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-        sqLiteDatabase.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT , PLACENAME TEXT, REVIEW TEXT, RATING TEXT)" );
+        //create table
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT ,PLACENAME TEXT, REVIEW TEXT, RATING TEXT)" );
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        //doesn't create a new table if it already exists
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(sqLiteDatabase);
 
