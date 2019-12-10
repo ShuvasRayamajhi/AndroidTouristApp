b0961a87f9f0c5addf4780e00a135ee633b46852
rayamajs <rayamajs@coventry.ac.uk>
2019-12-10 01:17:53 +0000

    casted and on click listener to insert

--- app/src/main/java/com/rayamajs/cwapp/review.java
+++ app/src/main/java/com/rayamajs/cwapp/review.java
@@ -3,6 +3,7 @@ package com.rayamajs.cwapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,7 +21,17 @@ public class review extends AppCompatActivity {
 
         myDb = new DatabaseHelper(this);
 
-        btn_insert = (Button) findViewById(R.id.btn_insert);
+        btn_insert = (Button) findViewById(R.id.btn_insert); //casting
+        text_PlaceName = (EditText) findViewById(R.id.editPlaceName);
+        text_Review = (EditText) findViewById(R.id.editReview);
+        text_Rating = (EditText) findViewById(R.id.editRating);
 
+        btn_insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isInserted =  myDb.insertData(text_PlaceName.getText().toString(), text_Review.getText().toString(), text_Rating.getText().toString()); //insert data from the xml fle
+
+            }
+        });
     }
 }
