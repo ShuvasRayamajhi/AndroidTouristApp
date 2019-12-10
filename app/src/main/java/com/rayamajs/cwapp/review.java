b8507a384b67bb470aa1f8c5c854c596480daa65
rayamajs <rayamajs@coventry.ac.uk>
2019-12-10 02:01:18 +0000

    can retrieve reviews from db

--- app/src/main/java/com/rayamajs/cwapp/review.java
+++ app/src/main/java/com/rayamajs/cwapp/review.java
@@ -1,7 +1,9 @@
 package com.rayamajs.cwapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +13,7 @@ import android.widget.Toast;
 public class review extends AppCompatActivity {
 
     DatabaseHelper myDb;
-    Button btn_insert;
+    Button btn_Insert, btn_View;
     EditText text_PlaceName, text_Review, text_Rating;
 
 
@@ -22,12 +24,13 @@ public class review extends AppCompatActivity {
 
         myDb = new DatabaseHelper(this);
 
-        btn_insert = (Button) findViewById(R.id.btn_insert); //casting
+        btn_Insert = (Button) findViewById(R.id.btn_insert);
+        btn_View = (Button) findViewById(R.id.btn_view);//casting
         text_PlaceName = (EditText) findViewById(R.id.editPlaceName);
         text_Review = (EditText) findViewById(R.id.editReview);
         text_Rating = (EditText) findViewById(R.id.editRating);
 
-        btn_insert.setOnClickListener(new View.OnClickListener() {
+        btn_Insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean isInserted =  myDb.insertData(text_PlaceName.getText().toString(), text_Review.getText().toString(), text_Rating.getText().toString()); //insert data from the xml fle
@@ -42,5 +45,37 @@ public class review extends AppCompatActivity {
 
             }
         });
+        btn_View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cur =  myDb.getAlldData();
+
+                if(cur.getCount()==0){
+                   showMessage("Error", "Data not found");
+                }
+
+                StringBuffer buffer = new StringBuffer();
+                while (cur.moveToNext()){
+
+                    buffer.append("ID: "+ cur.getString(0)+ "\n");
+                    buffer.append("Place Name: "+ cur.getString(1)+ "\n");
+                    buffer.append("Review: "+ cur.getString(2)+ "\n");
+                    buffer.append("Rating: "+ cur.getString(3)+ "\n");
+
+
+                }
+                showMessage("Reviews:", buffer.toString());
+
+            }
+        });
+
     }
+    public void showMessage (String title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
 }
